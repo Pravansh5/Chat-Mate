6912ab911a9e6248f000c7dda0be0293cdf0472b
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-04 15:53:42 +0530

    login-logout

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,12 +2,12 @@ import "dotenv/config"
 
 export const ENV={
     PORT:process.env.PORT,
-    Mongo_URI:process.env.Mongo_URI,
+    MONGO_URI:process.env.MONGO_URI,
     JWT_SECRET:process.env.JWT_SECRET,
     NODE_ENV:process.env.NODE_ENV,
     CLIENT_URL:process.env.CLIENT_URL,
     RESEND_API_KEY:process.env.RESEND_API_KEY,
     EMAIL_FROM:process.env.EMAIL_FROM,
-    EMAIL_FROM_NAME:process.env.EMAIL_FROM_NAME,
+    EMAIL_FROM_NAME:process.env.EMAIL_FROM_NAME
 
 }
