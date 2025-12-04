aa4aea851a9b4e8c9ea7377e76ec1b90faa77234
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-04 07:29:06 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { connectToDb } from './lib/db.js';
 dotenv.config()
 const app=express();
 const port=process.env.PORT||3000
+app.use(express.json());
+
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
-
+connectToDb()
 app.listen(port,()=>console.log(`Server is running on port http://127.0.0.1:${port}`));
