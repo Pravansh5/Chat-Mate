dbf96bb120eccb832da5fea45fdaf7c01b7a853e
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-07 16:59:49 +0530

    login-frontedn Implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set,get)=>({
     authUser:null,
     isCheckingAuth:true,
     isSigningUp:false,
+    isLoggingIn:false,
     checkAuth:async()=>{
         try {
             const res = await axiosInstance.get("/auth/check");
@@ -26,10 +27,39 @@ export const useAuthStore = create((set,get)=>({
             toast.success("Account created successfully!");
 
         } catch (error) {
-            toast.error(error)
+            const message = error.response?.data?.message || "SignUp failed";
+            toast.error(message);
             
         }finally{
             set({isSigningUp:false})
         }
+    
+    },
+    login:async(data)=>{
+        set({isLoggingIn:true})
+        try {
+            const res = await axiosInstance.post("/auth/login",data);
+            set({authUser:res.data});
+            toast.success("Logged in successfully!");
+
+        } catch (error) {
+            const message = error.response?.data?.message || "Login failed";
+            toast.error(message);
+
+            
+        }finally{
+            set({isLoggingIn:false})
+        }
+    
+    },
+      logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
     }
+  },
 }))
