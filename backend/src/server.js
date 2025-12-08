caf3a4f56666681039282532bb2ccce8e6180ca8
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-08 23:28:35 +0530

    online-user-implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,16 @@ import messageRoutes from "./routes/message.route.js"
 import { connectToDb } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import cookieParser from "cookie-parser"
+import {app,server} from "./lib/socket.js"
 import cors from 'cors'
 dotenv.config()
-const app=express();
 const port=ENV.PORT||3000
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
-connectToDb()
-app.listen(port,()=>console.log(`Server is running on port http://localhost:${port}`));
+server.listen(port,()=>{
+    console.log(`Server is running on port http://localhost:${port}`);
+    connectToDb()
+})
