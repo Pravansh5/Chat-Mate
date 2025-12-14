1a41a2e1ea947f777630225bd45db6af2dc151ef
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-14 10:21:13 +0530

    url update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:3000/api":"https://chat-mate-5aip.onrender.com/api",
+    baseURL:import.meta.env.MODE==="development"?"http://localhost:3000/api":"https://chat-mate-5aip.onrender.com",
     withCredentials:true,
 })
