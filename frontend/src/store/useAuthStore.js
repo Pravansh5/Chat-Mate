8d5690c77b1324dfb99011aea529d891ed19d577
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-14 10:16:37 +0530

    url-correction

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,10 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { useChatStore } from "./useChatStore";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://chat-mate-5aip.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
