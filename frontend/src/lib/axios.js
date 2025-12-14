6db2e69815bcbac32f9afc5b9968e8b816f8aae5
Pravansh5 <pravanshpatel5.pp@gmail.com>
2025-12-14 09:57:42 +0530

    api-correction

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:3000/api":"https://chat-mate-5aip.onrender.com",
+    baseURL:import.meta.env.MODE==="development"?"http://localhost:3000/api":"https://chat-mate-5aip.onrender.com/api",
     withCredentials:true,
 })
